e0d6f92eed228032d5b94feb4fcc8eabc49aea1d
senghak <seng48107@gmail.com>
2026-05-18 12:12:33 -0400

    Add LogisticRegression function for training

--- Titanic_Survivor.py
+++ Titanic_Survivor.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix  
 df = pd.read_csv("/Users/senghakheng/Documents/titanic_survivor/train.csv")
 
 df = df.drop("Cabin", axis = 1)
@@ -14,8 +16,17 @@ df['Embarked'] = df["Embarked"].map({ 'S': 0, 'C': 1, 'Q': 2 })
 # sns.countplot(x = 'Survived', hue = "Sex", data = df)
 # sns.countplot(x = 'Survived', hue = 'Pclass', data = df)
 # sns.histplot(x = 'Age', hue = 'Survived', data = df, bins = 30)
-plt.show()
-print(df.shape)
-print(df.head())
-print(df.info())
-print(df.describe())
+x = df.drop('Survived', axis = 1)
+y = df['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 33)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(model.score(x_train, y_train))
+print(model.score(x_test, y_test))
+
