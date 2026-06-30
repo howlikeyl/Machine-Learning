9b1ccc36839c4450dc3aa30bcb70dda768d85d88
NJ <seng48107@gmail.com>
2026-06-29 21:00:12 -0400

    Add Flask web server for remote robot control

---
+++ Picarx_robot/app.py
@@ -0,0 +1,55 @@
+from Movement import move, get_distance, move_camera
+from flask import Flask, Response, render_template
+from picamera2 import Picamera2
+import cv2
+import time
+
+camera = Picamera2()
+camera.configure(camera.create_preview_configuration(main={"size": (640, 480)}))
+camera.start()
+app = Flask(__name__)
+last_frame = None
+def capture_frame():
+    global last_frame
+    frame = camera.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # fix color
+    _,jpeg = cv2.imencode('.jpg', frame)
+    jpg_bytes = jpeg.tobytes()
+    last_frame = jpg_bytes
+    return last_frame
+
+def video_stream():
+    while True:
+        frame = capture_frame()
+        if frame:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + 
+                   frame + b'\r\n')
+        time.sleep(.03)
+@app.route('/move/<direction>')
+def handle_movement(direction):
+    if direction in ["forward", "backward", "left", "right", "stop", "backward_left", "backward_right"]:
+        move(direction)
+        return "Robot Moving"
+    else:
+        return "invalid"
+
+@app.route('/video')
+def video():
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/index')
+def index():
+    return render_template('Controller.html')
+@app.route('/camera/<direction>')
+def handle_camera(direction):
+    if direction in ["up", "down", "left", "right"]:
+        move_camera(direction)
+        return "camera moving"
+    else:
+        return "invalid"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+        
+
+          
